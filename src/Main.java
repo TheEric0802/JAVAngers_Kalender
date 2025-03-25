44d45384c4b1437055dd544285e9bc2f09758ece
Eric Schmidt <eric0802@web.de>
2025-03-25 15:44:04 +0100

    print dates

--- src/Main.java
+++ src/Main.java
@@ -20,8 +20,39 @@ public class Main {
 
         String calendarHead = "Mo Di Mi Do Fr Sa So";
 
+        int start = 0;
+
         for (String month : months.keySet()) {
             System.out.println("\n" + month + "\n" + calendarHead);
+
+            int count = 0;
+
+            for (int i = 1; i <= months.get(month); i++) {
+
+                if (i == 1) {
+                    count = 0;
+                    for (int j = 0; j < start; j++) {
+                        System.out.print("   ");
+                        count++;
+                    }
+                }
+
+                count++;
+
+                if (i < 10) {
+                    System.out.print(" ");
+                }
+
+                System.out.print(i + " ");
+
+                if (i == months.get(month)) {
+                    start = count % 7;
+                    System.out.println();
+                } else if (count % 7 == 0) {
+                    System.out.println();
+                }
+
+            }
         }
 
     }
