664bf0624d07659d064e816f07cc5168efd30125
Eric Schmidt <eric0802@web.de>
2025-03-25 14:40:33 +0100

    fill variable "month" with names and number of days

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,18 @@ public class Main {
 
         HashMap<String, Integer> months = new HashMap<>();
 
+        months.put("Januar", 31);
+        months.put("Februar", 28);
+        months.put("März", 31);
+        months.put("April", 30);
+        months.put("Mai", 31);
+        months.put("Juni", 30);
+        months.put("Juli", 31);
+        months.put("August", 31);
+        months.put("September", 30);
+        months.put("Oktober", 31);
+        months.put("November", 30);
+        months.put("Dezember", 31);
+
     }
 }
